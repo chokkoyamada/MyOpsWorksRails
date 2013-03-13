738c973e040aada2264ae02e5cfe6bdc7a3068be
Naoyuki Yamada <n.yamada@cyberagentamerica.com>
2013-03-13 18:54:10 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrails::Application.config.secret_token = 'a9e11411e5e8fb439fdf3ee9e3e164f64b338c3cc82905f0637bf884fcd628bcd06d7ea06153218fc5eec495ad9c51adcaf186191a2c8316761b2f249ac61e80'
